f5f95668c3263f67264c552847ce33beb79dc886
Josef Nagy <josef.nagy@seznam.cz>
2021-05-23 09:34:21 +0200

    feat()- added hover effect on hero title, and fixed issue of shadow possitioning after hover

--- components/home-page/hero.tsx
+++ components/home-page/hero.tsx
@@ -5,10 +5,12 @@ const Hero: NextPage = () => {
     <>
       <section className="h-full grid place-items-center uppercase px-4" id="hero">
         <h1 className="text-header header text-background leading-heading text-center font-extrabold tracking-wider">
-          Hello,
-          <br /> I am Josef.
+          <span className="transition-colors duration-500 hover:text-white">Hello,</span>
+          <br /> <span className="transition-colors duration-500 hover:text-white">I</span>{' '}
+          <span className="transition-colors duration-500 hover:text-white">am</span>{' '}
+          <span className="transition-colors duration-500 hover:text-white">Josef.</span>
           <br />
-          Welcome.
+          <span className="transition-colors duration-500 hover:text-white">Welcome.</span>
         </h1>
       </section>
     </>
