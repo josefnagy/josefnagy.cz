59cb62b81621244813ef1a8b24dab60957e40e82
Josef Nagy <josef.nagy@seznam.cz>
2021-05-24 13:13:45 +0200

    feat()- added hero and header animation on first load

--- components/home-page/hero.tsx
+++ components/home-page/hero.tsx
@@ -1,17 +1,72 @@
 import { NextPage } from 'next'
+import { motion } from 'framer-motion'
+
+const hero = {
+  hidden: {
+    y: '15px',
+    opacity: 0,
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      // type: 'tween',
+      delay: 0.3,
+      duration: 0.7,
+      // when: 'beforeChildren',
+      staggerChildren: 0.4,
+      delayChildren: 0.2,
+    },
+  },
+}
+
+const text = {
+  hidden: {
+    y: '10px',
+    opacity: 0,
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      type: 'tween',
+      duration: 0.8,
+    },
+  },
+}
 
 const Hero: NextPage = () => {
   return (
     <>
       <section className="h-screen grid place-items-center uppercase px-4" id="hero">
-        <h1 className="text-header header text-background leading-heading text-center font-extrabold tracking-wider">
-          <span className="transition-colors duration-500 hover:text-white">Hello,</span>
-          <br /> <span className="transition-colors duration-500 hover:text-white">I</span>{' '}
-          <span className="transition-colors duration-500 hover:text-white">am</span>{' '}
-          <span className="transition-colors duration-500 hover:text-white">Josef.</span>
+        <motion.h1
+          className="relative text-header header text-background leading-heading text-center font-extrabold tracking-wider"
+          variants={hero}
+          initial="hidden"
+          animate="visible"
+        >
+          <motion.span
+            className="inline-block transition-colors duration-500 hover:text-white"
+            variants={text}
+          >
+            Hello,
+          </motion.span>
+          <br />{' '}
+          <motion.span className="inline-block" variants={text}>
+            <span className="inline-block transition-colors duration-500 hover:text-white">I</span>{' '}
+            <span className="inline-block transition-colors duration-500 hover:text-white">am</span>{' '}
+            <span className="inline-block transition-colors duration-500 hover:text-white">
+              Josef.
+            </span>
+          </motion.span>
           <br />
-          <span className="transition-colors duration-500 hover:text-white">Welcome.</span>
-        </h1>
+          <motion.span
+            className="inline-block transition-colors duration-500 hover:text-white"
+            variants={text}
+          >
+            Welcome.
+          </motion.span>
+        </motion.h1>
       </section>
     </>
   )
--- components/layout/main-navigation.tsx
+++ components/layout/main-navigation.tsx
@@ -1,13 +1,35 @@
 import { NextPage } from 'next'
 import Link from 'next/link'
 import Burger from '../ui/burger'
+import { motion } from 'framer-motion'
 
 import Logo from './logo'
+
+const header = {
+  start: {
+    y: '-50px',
+    opacity: 0,
+  },
+  end: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      type: 'tween',
+      delay: 2,
+      duration: 1,
+    },
+  },
+}
 
 const MainNavigation: NextPage = () => {
   return (
     <>
-      <header className="px-4 pt-10 w-full flex flex-row justify-between flex-initial fixed z-20">
+      <motion.header
+        className="inline-block px-4 pt-10 w-full flex flex-row justify-between flex-initial fixed z-20"
+        variants={header}
+        initial="start"
+        animate="end"
+      >
         <Link href="/">
           <a className="ml-28 logo z-30">
             <Logo />
@@ -16,7 +38,7 @@ const MainNavigation: NextPage = () => {
         <nav className="self-center grid place-items-center mr-28">
           <Burger />
         </nav>
-      </header>
+      </motion.header>
     </>
   )
 }
